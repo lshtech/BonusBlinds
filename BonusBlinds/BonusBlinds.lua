5870ec122591154ea89b264125444127d5b702bd
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2024-07-17 16:03:45 -0400

    Update BonusBlinds.lua

--- BonusBlinds/BonusBlinds.lua
+++ BonusBlinds/BonusBlinds.lua
@@ -2168,7 +2168,7 @@ G.FUNCS.super_reroll_shop = function(e)
           play_sound('other1')
           
           G.GAME.current_round.used_packs = {}
-          for i = 1, G.GAME.modifiers.cry_booster_packs or 2 do
+          for i = 1, 2 do
             G.GAME.current_round.used_packs = G.GAME.current_round.used_packs or {}
             if not G.GAME.current_round.used_packs[i] then
                 G.GAME.current_round.used_packs[i] = get_pack('shop_pack').key
@@ -2177,10 +2177,6 @@ G.FUNCS.super_reroll_shop = function(e)
             if G.GAME.current_round.used_packs[i] ~= 'USED' then 
                 local card = Card(G.shop_booster.T.x + G.shop_booster.T.w/2,
                 G.shop_booster.T.y, G.CARD_W*(G.P_CENTERS[G.GAME.current_round.used_packs[i]].set == 'Booster' and 1.27 or 1), G.CARD_H*(G.P_CENTERS[G.GAME.current_round.used_packs[i]].set == 'Booster' and 1.27 or 1), G.P_CARDS.empty, G.P_CENTERS[G.GAME.current_round.used_packs[i]], {bypass_discovery_center = true, bypass_discovery_ui = true})
-                if G.GAME.modifiers.cry_enable_flipped_in_shop and pseudorandom('cry_flip_pack'..G.GAME.round_resets.ante) > 0.7 then
-                    card.cry_flipped = true
-                end
-                cry_misprintize(card)
                                                                 create_shop_card_ui(card, 'Booster', G.shop_booster)
                 card.ability.booster_pos = i
                 card:start_materialize()
