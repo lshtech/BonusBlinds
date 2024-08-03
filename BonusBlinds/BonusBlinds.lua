18422c212ff8bc50c5cbabefd813646ec6f94f9f
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2024-08-02 21:22:55 -0400

    Add files via upload

--- BonusBlinds/BonusBlinds.lua
+++ BonusBlinds/BonusBlinds.lua
@@ -4,7 +4,7 @@
 --- PREFIX: bb
 --- MOD_AUTHOR: [mathguy]
 --- MOD_DESCRIPTION: Bonus Blinds
---- VERSION: 1.5.2
+--- VERSION: 1.5.1
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
